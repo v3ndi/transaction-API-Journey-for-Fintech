66348e47b0fb41e5991adc7d1be7c733b836ca9f
v3ndi <shouravislam666@gmail.com>
2023-11-26 13:48:04 +0600

    upload

---
+++ report.js
@@ -0,0 +1,18 @@
+const newman = require('newman');
+require('dotenv').config();
+ 
+newman.run({
+    //collection: require('./collection/dmoney.json'),
+    collection:`https://api.postman.com/collections/31247775-b63bd418-514e-4848-a070-6f5509a7517e?access_key=${process.env.key}`,
+    //environment:require('./collection/env.json'),
+    reporters: 'htmlextra',
+    iterationCount: 1,
+    reporter: {
+        htmlextra: {
+            export: './Reports/report.html', // If not specified, the file will be written to `newman/` in the current working directory.
+        }
+    }
+}, function (err) {
+    if (err) { throw err; }
+    console.log('collection run complete!');
+});
